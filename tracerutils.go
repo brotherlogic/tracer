b23fc12053fcc3fa90dda38c4cacaaa39dd3d963
Simon Tucker <simon@brotherlogic@gmail>
2018-11-05 14:31:40 -0800

    Adds a mutex on get long calls. This closes #45

--- tracerutils.go
+++ tracerutils.go
@@ -5,6 +5,8 @@ import pb "github.com/brotherlogic/tracer/proto"
 func (s *Server) getLongContextCall() *pb.ContextCall {
 	var rcall *pb.ContextCall
 	longest := int64(0)
+	s.callsMutex.Lock()
+	defer s.callsMutex.Unlock()
 	for _, call := range s.calls {
 		if call.Properties.Died > 0 && call.Properties.Created > 0 {
 			took := call.Properties.Died - call.Properties.Created
