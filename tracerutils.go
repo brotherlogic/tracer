b4f8f36493962ee1ddffa586163ac8e1c188d12b
Simon Tucker <simon@brotherlogic@gmail>
2019-03-08 17:27:28 -0800

    Adds some processing of traces. This closes #177.

--- tracerutils.go
+++ tracerutils.go
@@ -1,7 +1,40 @@
 package main
 
-import "golang.org/x/net/context"
+import (
+	"time"
+
+	"golang.org/x/net/context"
+)
 
 func (s *Server) clean(ctx context.Context) {
+	process := s.calls
 	s.calls = nil
+
+	for _, events := range process {
+		eventStart := ""
+		times := time.Now().Unix()
+
+		for _, ev := range events.Events {
+			if ev.Timestamp < times {
+				times = ev.Timestamp
+				eventStart = ev.Call
+			}
+		}
+
+		s.counts[eventStart]++
+	}
+
+	most := ""
+	mostCalls := 0
+	allCalls := 0
+	for c, count := range s.counts {
+		allCalls += count
+		if count > mostCalls {
+			most = c
+			mostCalls = count
+		}
+	}
+
+	s.allCalls = int64(allCalls)
+	s.mostCalled = most
 }
