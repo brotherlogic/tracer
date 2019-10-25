68e2468de912bc98f59735b49455988d49bf98f7
Simon Tucker <simon@brotherlogic@gmail>
2019-10-25 00:15:16 -0700

    Wha

--- tracerutils.go
+++ tracerutils.go
@@ -15,8 +15,8 @@ func (s *Server) clean(ctx context.Context) error {
 	for _, events := range process {
 
 		for _, marked := range s.markedIds {
-			s.message = fmt.Sprintf("Mark = %v -> %v", marked.LongRunningId, events.Events[0].Id)
 			if events.Events[0].Id == marked.LongRunningId {
+				s.message = fmt.Sprintf("Mark = %v -> %v", marked.LongRunningId, events.Events[0].Id)
 				list := fmt.Sprintf("%v/%v %v %v %v [%v]", events.Events[0].Server, events.Events[0].Binary, events.Events[0].Timestamp, events.Events[0].Call, time.Millisecond*time.Duration(marked.RunningTimeInMs), marked.Request)
 				for _, ev := range events.Events[1:] {
 					list += "\n" + fmt.Sprintf("%v/%v %v %v", ev.Server, ev.Binary, time.Millisecond*time.Duration(ev.Timestamp-events.Events[0].Timestamp), ev.Call)
